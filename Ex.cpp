c0af1221875ba329ec13ce7730f5caf2c5fa5832
Pongpx <yannafong555@gmail.com>
2024-02-18 23:56:51 +0700

    sa

--- Ex.cpp
+++ Ex.cpp
@@ -6,23 +6,52 @@
 
 using namespace std;
 
+string text[26] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+string aws1[] = {"Capital cities.txt","Countries.txt","Land Animals.txt","Musical Instruments.txt","Flowers.txt","Perfume Brands.txt","Languages.txt","Ocean Animals.txt","Electronic Gadgets.txt","Cooking Utensils.txt"};
+
 int main() {
     srand(time(0));
 
-    string text[26] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    string textline;
-    int st,bt;
+    ifstream qs;
+    ifstream checkaws;
+    ofstream dest;
+    string tl;
+    string tla;
+    string aws;
+    int st = 1;
+    int bt,score;
+    
+    while(st == 1){
+        qs.open("Question\\1st_Q-list.txt");
+        dest.open("Answer_now");
 
-    //cin >> bt;
+        cout << "pls Enter number 1 : ";
+        cin >> bt;
+        cout << "-----------------------------------"<< endl;
 
-    /*if(bt == 1){
-        int x = rand()%26;
-        cout << text[x];
-    }*/
-    ifstream source("1st_Q-list.text");
-    while(getline(source, textline)){
-        cout << textline << '\n';
+        if(bt == 1){
+            int x = rand()%26;
+            cout << "your letter is "<< text[x] << endl;
+            cout << "*-----------------------*" << endl;
+        }
+        // **countdown timer**
+
+        while(getline(qs, tl)){
+            cout << tl << ' ';
+            cin >> aws;
+            dest << aws << endl;
+        }
+
+        qs.close();
+        dest.close();
+
+        //**check aws**
+
+
+        cout << "-----------------------------------"<< endl;
+
+        //-------------------------------------------------------------------
+        st = 0;
     }
-
     return 0;
 }
