4aec0874825fcc764695c9fa5d97b585484e6b1b
Pongpx <yannafong555@gmail.com>
2024-02-18 17:31:20 +0700

    code update but its need some fix bc code its not work have bug.

--- Ex.cpp
+++ Ex.cpp
@@ -2,17 +2,26 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-    int st,bt;
-    cin >> bt;
     srand(time(0));
+
     string text[26] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    if(bt == 1 and st == 0){
+    string textline;
+    int st,bt;
+
+    //cin >> bt;
+
+    /*if(bt == 1){
         int x = rand()%26;
         cout << text[x];
+    }*/
+    ifstream source("1st_Q-list.text");
+    while(getline(source, textline)){
+        cout << textline << '\n';
     }
 
     return 0;
