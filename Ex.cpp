88f1c0c54df49b28bbc548a5a2b05dfb2ab8d1b1
Pongpx <yannafong555@gmail.com>
2024-02-19 20:29:07 +0700

    sa

--- Ex.cpp
+++ Ex.cpp
@@ -3,27 +3,27 @@
 #include <ctime>
 #include <cstdlib>
 #include <fstream>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
 string text[26] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-string aws1[] = {"Capital cities.txt","Countries.txt","Land Animals.txt","Musical Instruments.txt","Flowers.txt","Perfume Brands.txt","Languages.txt","Ocean Animals.txt","Electronic Gadgets.txt","Cooking Utensils.txt"};
+string aws1[10] = {"Capital cities.txt","Countries.txt","Land Animals.txt","Musical Instruments.txt","Flowers.txt","Perfume Brands.txt","Languages.txt","Ocean Animals.txt","Electronic Gadgets.txt","Cooking Utensils.txt"};
 
 int main() {
     srand(time(0));
 
-    ifstream qs;
-    ifstream checkaws;
+    ifstream qs,checkaws,aws_now;
     ofstream dest;
-    string tl;
-    string tla;
-    string aws;
+    string tl,aws,tl_now;
     int st = 1;
-    int bt,score;
-    
+    int bt = 0;
+    int score = 0;
+
     while(st == 1){
         qs.open("Question\\1st_Q-list.txt");
-        dest.open("Answer_now");
+        dest.open("Answer_now.txt");
 
         cout << "pls Enter number 1 : ";
         cin >> bt;
@@ -31,8 +31,10 @@ int main() {
 
         if(bt == 1){
             int x = rand()%26;
-            cout << "your letter is "<< text[x] << endl;
-            cout << "*-----------------------*" << endl;
+            cout <<  "*-----------------------*" << endl;
+            cout << endl << "your letter is "<< text[x] << endl;
+            cout << endl << "*-----------------------*" << endl;
+            cout << endl ;
         }
         // **countdown timer**
 
@@ -45,10 +47,30 @@ int main() {
         qs.close();
         dest.close();
 
-        //**check aws**
+        cout << endl <<"*-----------------------*" << endl;
+        cout << endl ;
 
+        for(int i = 0; i < 10 ; i++){
+            aws_now.open("Answer_now.txt");
+            checkaws.open("Answer\\"+aws1[i]);
+            cout << i+1 << ". ";
+            while(getline(aws_now,tl_now)){
+                while(getline(checkaws,tl)){
+                    if(tl == tl_now){
+                        cout << tl_now << " +1score" << " ";
+                        score++ ;
+                    }
+                }
+            }
+            checkaws.close();
+            aws_now.close();
+            
+        }
 
-        cout << "-----------------------------------"<< endl;
+        cout << endl << "*-----------------------*" << endl;
+        cout << endl <<"your score is : " << score << endl;
+
+        cout << endl << "-----------------------------------"<< endl;
 
         //-------------------------------------------------------------------
         st = 0;
