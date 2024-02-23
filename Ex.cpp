dec5e7f58cb3485bc6144fae5234a0aa13a2a7fa
gun <asdaf@gmail.com>
2024-02-23 14:56:07 +0700

    gun again

--- Ex.cpp
+++ Ex.cpp
@@ -10,7 +10,10 @@ using namespace std;
 using namespace std::chrono;
 
 string text[26] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+string ques[3] = {"1st_Q-list.txt","2nd_Q-list.txt","3rd_Q-list.txt"};
 string aws1[10] = {"Capital cities.txt","Countries.txt","Land Animals.txt","Musical Instruments.txt","Flowers.txt","Perfume Brands.txt","Languages.txt","Ocean Animals.txt","Electronic Gadgets.txt","Cooking Utensils.txt"};
+string aws2[10] = {"Body parts.txt","Colors.txt","Programming Languages.txt","Gemstones.txt","Disney Character Names.txt","Video Games.txt","Job titles.txt","Holidays.txt","Celebrities.txt","Indoors and Outdoor Hobbies.txt"};
+string aws3[10] = {"Sports.txt","Vegetables.txt","Subjects.txt","Construction.txt","Thing in a museum.txt","Board Games.txt","Ice cream flavors.txt","Clothing Brands.txt","Fruits.txt","Car brands.txt"};
 
 void Countdown_Timer(int seconds){
     if(seconds <= 0){
@@ -31,13 +34,30 @@ int main() {
     ofstream dest;
     string tl,aws,;
     string ans_arch[];
+    string q_ans[];
     int st = 1;
     int bt = 0;
     int sec = 0;
     int score = 0;
 
     while(st == 1){
-        qs.open("Question\\1st_Q-list.txt");
+        int q = rand()%3;
+
+        if( q == 0 ){
+            for(int i = 0; i < 10; i++ ){
+                q_ans[i] = aws1[i];
+            }
+        }else if( q == 1 ){
+            for(int i = 0; i < 10; i++ ){
+                q_ans[i] = aws2[i];
+            }
+        }else if( q == 2 ){
+            for(int i = 0; i < 10; i++ ){
+                q_ans[i] = aws3[i];
+            }
+        }
+        
+        qs.open("Question\\"+ques[q]);
         dest.open("Answer_now.txt");
 
         cout << "pls Enter number 1 : ";
@@ -74,10 +94,10 @@ int main() {
         }
         qs.close();
 
-        for(int i = 0; i < 10 ; i++){
-            checkaws.open("Answer\\"+aws1[i]);
+        for(int i = 0; i < ans_arch.size(); i++){
+            checkaws.open("Answer\\"+q_ans[i]);
             cout << i+1 << ". ";
-            for(int j = 0; j < ans_arch.size();j++){
+            for(int j = 0; j < ans_arch.size(); j++){
                 while(getline(checkaws,tl)){
                     if(tl == ans_arch[j]){
                         cout << tl_now << " +1score" << " ";
@@ -90,10 +110,8 @@ int main() {
 
         cout << endl << "*-----------------------*" << endl;
         cout << endl <<"your score is : " << score << endl;
-
         cout << endl << "-----------------------------------"<< endl;
 
-        //-------------------------------------------------------------------
         st = 0;
     }
     return 0;
