f8313809102ca70961dc16583a0e825f869b5a80
krzers <tyhffggfg@gmail.com>
2024-03-03 00:46:44 +0700

    window

--- window_test/window_screen.cpp
+++ window_test/window_screen.cpp
@@ -1,9 +1,44 @@
 #include <windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    MessageBox(NULL, "HELLO","My first GUI",MB_OK);
+    WNDCLASSW wc = {0};
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInstance; // Corrected hInstance assignment
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc = WindowProcedure;
+
+    if (!RegisterClassW(&wc))  
+        return -1;
+
+    HWND hWnd = CreateWindowW(L"myWindowClass", L"Scatter Glory Chaos", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL); // Corrected hInstance usage
+
+    if (hWnd == NULL) // Check if window creation failed
+        return -1;
+
+    MSG msg = {0};
+
+    while (GetMessage(&msg, NULL, 0, 0)) // Corrected GetMessage parameters
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
 
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
+    return 0;
+}
