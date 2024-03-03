50a46ad25dc4b3464a61fa30bf1ff6f35b868721
Pongpx <yannafong555@gmail.com>
2024-03-03 22:52:21 +0700

    pap die round 254

--- Ex.cpp
+++ Ex.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <thread>
 #include <vector>
+#include <conio.h>
 
 using namespace std::chrono;
 using namespace std;
@@ -16,6 +17,7 @@ string ques[3] = {"1st_Q-list.txt","2nd_Q-list.txt","3rd_Q-list.txt"};
 string aws1[10] = {"Capital cities.txt","Countries.txt","Land Animals.txt","Musical Instruments.txt","Flowers.txt","Perfume Brands.txt","Languages.txt","Ocean Animals.txt","Electronic Gadgets.txt","Cooking Utensils.txt"};
 string aws2[10] = {"Body parts.txt","Colors.txt","Programming Languages.txt","Gemstones.txt","Disney Character Names.txt","Video Games.txt","Job titles.txt","Holidays.txt","Celebrities.txt","Indoors and Outdoor Hobbies.txt"};
 string aws3[10] = {"Sports.txt","Vegetables.txt","Subjects.txt","Construction.txt","Thing in a museum.txt","Board Games.txt","Ice cream flavors.txt","Clothing Brands.txt","Fruits.txt","Car brands.txt"};
+string tl,aws,name;
 vector<string> ans_arch;
 vector<string> q_ans;
 vector<string> name_ply;
@@ -23,7 +25,6 @@ int r = 0;
 int score = 0;
 int n_name = 0;
 int q = 0;
-string tl,aws,name;
 
 char random_letter(){
     return 'A' + rand()%26;
@@ -82,6 +83,7 @@ void answer_ply(){
 }
 
 void Countdown_Timer(int seconds){
+    if(r == 1){
         if(seconds <= 0){
             cout << endl << endl << "Time's up !!!  T_T" << endl;
             ::r = 0;
@@ -89,8 +91,27 @@ void Countdown_Timer(int seconds){
         }
         this_thread::sleep_for(std::chrono::seconds(1));
         Countdown_Timer(seconds -1);
-    
+    }
 }
+
+/*void Countdown_Timer(int seconds){
+    int remaining_time = seconds;
+    time_t start_time = time(NULL);
+    while (remaining_time > 0){
+        if(r == 1){
+            time_t current_time = time(NULL);
+            int elapsed_seconds = difftime(current_time, start_time);
+            if (elapsed_seconds >= remaining_time)
+            {
+                cout << endl << "TIME'S UP!!!" << endl;
+                ::r = 0;
+                break;
+            }
+            remaining_time -= elapsed_seconds;
+            start_time = current_time;
+        }
+    }
+}*/
 
 int main() {
     srand(time(0));
@@ -116,14 +137,16 @@ int main() {
 
             cout << "Enter time (sec) : ";
             cin >> sec;
-            ::r = sec;
             cout << endl;
 
             for(int n = 0 ; n < n_name ; n++){
+                ::r = 1;
                 cout << "Round "<< n+1 << " player : "<< name_ply[n] << endl;
                 cout << endl;
-                //thread taskCD(Countdown_Timer, sec);
-                cout << r << " " << "SECOND LEFT!!!!" << endl << endl;
+
+                thread task_cd(Countdown_Timer,sec);
+
+                cout << sec << " " << "SECOND LEFT!!!!" << endl << endl;
 
                 random_question();
                 answer_ply();
@@ -133,7 +156,7 @@ int main() {
                 cout << endl << "-----------------------------------"<< endl;
 
                 ::ans_arch.clear();
-                ::r = sec;
+                ::r = 0;
                 score = 0;
             }
         }
