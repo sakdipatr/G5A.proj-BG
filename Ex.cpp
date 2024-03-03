6d06b554924928f52aa4204acd8377b4652a59dd
Pongpx <yannafong555@gmail.com>
2024-03-03 20:10:15 +0700

    c_now

--- Ex.cpp
+++ Ex.cpp
@@ -15,17 +15,15 @@ string ques[3] = {"1st_Q-list.txt","2nd_Q-list.txt","3rd_Q-list.txt"};
 string aws1[10] = {"Capital cities.txt","Countries.txt","Land Animals.txt","Musical Instruments.txt","Flowers.txt","Perfume Brands.txt","Languages.txt","Ocean Animals.txt","Electronic Gadgets.txt","Cooking Utensils.txt"};
 string aws2[10] = {"Body parts.txt","Colors.txt","Programming Languages.txt","Gemstones.txt","Disney Character Names.txt","Video Games.txt","Job titles.txt","Holidays.txt","Celebrities.txt","Indoors and Outdoor Hobbies.txt"};
 string aws3[10] = {"Sports.txt","Vegetables.txt","Subjects.txt","Construction.txt","Thing in a museum.txt","Board Games.txt","Ice cream flavors.txt","Clothing Brands.txt","Fruits.txt","Car brands.txt"};
-int r =1;
+int r = 0;
+int n = 1;
 
 void Countdown_Timer(int seconds){
     if(seconds <= 0){
-        cout << endl << "Time's up." << endl;
+        cout << endl << endl << "Time's up !!!  T_T" << endl;
         ::r = 0;
         return;
-    }
-    if(seconds==10){
-        cout << seconds << "SECOND LEFT!!!!" << endl;
-    }
+    }if(n == 0) return;
     this_thread::sleep_for(std::chrono::seconds(1));
     Countdown_Timer(seconds -1);
 }
@@ -46,32 +44,34 @@ int main() {
 
     while(st == 1){
 
-        cout << "pls Enter number 1 : ";
+        cout << "Enter number 1 for strat : ";
         cin >> bt;
-        cout << "-----------------------------------"<< endl;
+        cout << endl;
 
         if(bt == 1){
-            cout << "player = ";
+            cout << "Number of players = ";
             cin >> n_name;
             for (int i = 0 ; i < n_name; i++){
-                cout << "name player " << i+1 << " = ";
+                cout << "Name player " << i+1 << " = ";
                 cin >> name;
                 name_ply.push_back(name);
             }
             int x = rand()%26;
             cout <<  "*-----------------------*" << endl;
-            cout << endl << "your letter is "<< letters[x] << endl;
+            cout << endl << "Your letter is "<< letters[x] << endl;
             cout << endl << "*-----------------------*" << endl;
             cout << endl ;
             cout << "Enter time (sec) : ";
             cin >> sec;
-            cout << endl << "*-----------------------*" << endl;
-            thread taskCD(Countdown_Timer, sec);
-            // cd timer
+            ::r = sec;
+            cout << endl;
             for(int n = 0 ; n < n_name ; n++){
-                cout << "round "<< n+1 << " player : "<< name_ply[n] << endl;
+                cout << "Round "<< n+1 << " player : "<< name_ply[n] << endl;
                 cout << endl;
 
+                thread taskCD(Countdown_Timer, sec);
+
+                cout << r << " " << "SECOND LEFT!!!!" << endl << endl;
                 int q = rand()%3;
                 if( q == 0 ){
                     for(int i = 0; i < 10; i++ ){
@@ -93,8 +93,8 @@ int main() {
                 while(getline(qs, tl)){
                     cout << tl << ' ';
                     cin >> aws;
+                    if(r == 0) break;
                     ans_arch.push_back(aws);
-                    if(r != 1) break;
                 }
 
                 qs.close();
@@ -103,7 +103,7 @@ int main() {
                 cout << endl ;
 
                 //เช็คคะแนน
-                for(int i = 0; i < 10; i++){
+                for(int i = 0; i < ans_arch.size(); i++){
                     checkaws.open("Answer\\"+q_ans[i]);
                     cout << i+1 << ". ";
                     while(getline(checkaws,tl)){
@@ -116,9 +116,11 @@ int main() {
                 }
 
                 cout << endl << "*-----------------------*" << endl;
-                cout << endl << name_ply[n]<<" score is : " << score << endl << sec << " " << r << " ";
+                cout << endl << name_ply[n]<<" score is : " << score << endl;
                 cout << endl << "-----------------------------------"<< endl;
 
+                ans_arch.clear();
+                ::r = sec;
                 score = 0;
             }
         }
